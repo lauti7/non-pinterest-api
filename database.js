ec23ff514dbe71c77087835868db727c86ca4f77
Lautaro Bustos <lautarobustos@MacBook-Air-de-Lautaro.local>
2019-07-30 19:51:08 -0300

    third commit

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const connection = new Sequelize('pinterest', 'root', '', {
-  host: 'localhost',
+  host: 'mysql://b9ec223f33c1d2:b4500ad0@us-cdbr-iron-east-02.cleardb.net/heroku_e48b4d49374d9d3?reconnect=true',
   dialect: 'mysql'
 });
 
