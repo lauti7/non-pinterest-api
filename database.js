f51656f3916a3888aa665cc559ee8c6d02137e32
Lautaro Bustos <lautarobustos@MacBook-Air-de-Lautaro.local>
2019-08-02 20:28:33 -0300

    add an or in db congi

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize(process.env.CLEARDB_DATABASE_NAME, process.env.CLEARDB_DATABASE_USER, process.env.CLEARDB_DATABASE_PASSWORD, {
-  host: process.env.CLEARDB_DATABASE_HOST,
+const connection = new Sequelize(process.env.CLEARDB_DATABASE_NAME || 'pinterest', process.env.CLEARDB_DATABASE_USER || 'root', process.env.CLEARDB_DATABASE_PASSWORD || '', {
+  host: process.env.CLEARDB_DATABASE_HOST || '127.0.0.1',
   dialect: 'mysql'
 });
 
