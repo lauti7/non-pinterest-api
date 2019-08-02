63c02dc43c4205d993536b4863032da6b17cff0e
Lautaro Bustos <lautarobustos@MacBook-Air-de-Lautaro.local>
2019-08-02 20:21:10 -0300

    changes in db

--- database.js
+++ database.js
@@ -1,13 +1,8 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('heroku_e48b4d49374d9d3', 'b9ec223f33c1d2', 'b4500ad0', {
-  host: 'us-cdbr-iron-east-02.cleardb.net',
+const connection = new Sequelize(process.env.CLEARDB_DATABASE_NAME, process.env.CLEARDB_DATABASE_USER, process.env.CLEARDB_DATABASE_PASSWORD, {
+  host: process.env.CLEARDB_DATABASE_HOST,
   dialect: 'mysql'
 });
-//
-// const connection = new Sequelize('pinterest', 'root', '', {
-//   host: '127.0.0.1',
-//   dialect: 'mysql'
-// });
 
 module.exports = connection
